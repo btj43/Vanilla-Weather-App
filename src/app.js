04fe72f534b210fd3afc296a90bd24de32402bca
btj43 <122530765+btj43@users.noreply.github.com>
2023-02-17 23:13:01 +0000

    Added more fields from openweather

--- src/app.js
+++ src/app.js
@@ -29,16 +29,20 @@ liveDate.innerHTML = todayDate(currentTime);
 
 //want to get data from openweather then push into 4 different places
 function displayTemp(response) {
+  let cityName = document.querySelector("#city");
+  let description = document.querySelector("#description");
   let temperature = document.querySelector("#temp");
   let windSpeed = document.querySelector("#wind");
   let humidity = document.querySelector("#humidity");
+  cityName.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  description.innerHTML = response.data.weather[0].description;
   temperature.innerHTML = Math.round(response.data.main.temp);
   windSpeed.innerHTML = `${Math.round(response.data.wind.speed)} m/s`;
   humidity.innerHTML = Math.round(response.data.main.humidity);
 }
 
-let cityName = "London";
+let city = "London";
 let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(url).then(displayTemp);
