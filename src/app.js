b4c618de73c01350d2ff4bae871df45294ee4af0
btj43 <122530765+btj43@users.noreply.github.com>
2023-02-17 17:58:57 +0000

    Pull data from OpenWeather API

--- src/app.js
+++ src/app.js
@@ -26,3 +26,19 @@ function todayDate(date) {
 let currentTime = new Date();
 let liveDate = document.querySelector("#date");
 liveDate.innerHTML = todayDate(currentTime);
+
+//want to get data from openweather then push into 4 different places
+function displayTemp(response) {
+  let temperature = document.querySelector("#temp");
+  let windSpeed = document.querySelector("#wind");
+  let humidity = document.querySelector("#humidity");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  windSpeed.innerHTML = `${Math.round(response.data.wind.speed)} m/s`;
+  humidity.innerHTML = Math.round(response.data.main.humidity);
+}
+
+let cityName = "London";
+let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+axios.get(url).then(displayTemp);
